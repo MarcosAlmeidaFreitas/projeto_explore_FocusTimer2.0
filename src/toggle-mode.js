0af3120b1c7b5cd00586628062c62688194aad40
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2023-10-26 11:31:51 -0300

    buttons

--- src/toggle-mode.js
+++ src/toggle-mode.js
@@ -1,8 +1,13 @@
 let darkMode = false;
 const toggleButton = document.getElementById('toggle-button');
 
-toggleButton.addEventListener('click', () =>{
-  document.documentElement.classList.toggle('dark')
-  darkMode = !darkMode;
+toggleButton.addEventListener('click', (event) =>{
+  document.documentElement.classList.toggle('dark');
+  
+  let darkOrLight = darkMode ? 'Dark' : 'Light';
+  let message = `Ativar ${darkOrLight} Mode`;
+  
+  event.currentTarget.querySelector('span').textContent = message; 
 
+  darkMode = !darkMode;
 })
