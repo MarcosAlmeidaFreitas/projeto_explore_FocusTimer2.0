ac515d59468998f23f3e9fc5be9cd869843b1821
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2023-10-28 07:08:18 -0300

    songs play pause ok

--- src/controlsSongs.js
+++ src/controlsSongs.js
@@ -1,24 +1,56 @@
 import * as songs from './songs.js'
+import state from './state.js';
 
-const controlSongs =  document.getElementById('buttonSongs');
+const controlSongs =  document.querySelector('.buttonSongs');
 
 controlSongs.addEventListener('click', (event) => {
-    let action = event.target.dataset.action;
-    if(action == undefined){
-      return;
-    }
+  // Pega a ação do evento que foi clicado, e se for undefined ele termina a função
+  let action = event.target.dataset.action;
+  if(action == undefined){
+    return;
+  }
 
-    if(action == 'playFlorest'){
-      songs.florest.play();
-      event.target.classList.toggle('buttonSongSelected');
-    }else if(action == 'playRain'){
-      songs.rain.play();
-    }else if(action == 'playCoffeeShop'){
-      songs.coffeeShop.play();
-    }else{
-      songs.campFire.play();
-    }
+  if(state.isMute == true){
+    state.isMute = false;
+    state.nameSong = action;
+    playSong(event, action);
+  }else{
+    stopSong(event, action);
+    state.isMute = true;
+  }
 
-    console.log(action);
-  });
+  
+  console.log('name song is: ' + state.nameSong);
+});
 
+function playSong(event, action){
+  if(action == 'playFlorest'){
+    songs.florest.play();
+    event.target.classList.toggle('buttonSongSelected');
+  }else if(action == 'playRain'){
+    songs.rain.play();
+    event.target.classList.toggle('buttonSongSelected');
+  }else if(action == 'playCoffeeShop'){
+    songs.coffeeShop.play();
+    event.target.classList.toggle('buttonSongSelected');
+  }else{
+    songs.campFire.play();
+    event.target.classList.toggle('buttonSongSelected');
+  }
+}
+
+function stopSong(event, action){
+  if(action == 'playFlorest'){
+    songs.florest.pause();
+    event.target.classList.toggle('buttonSongSelected');
+  }else if(action == 'playRain'){
+    songs.rain.pause();
+    event.target.classList.toggle('buttonSongSelected');
+  }else if(action == 'playCoffeeShop'){
+    songs.coffeeShop.pause();
+    event.target.classList.toggle('buttonSongSelected');
+  }else{
+    songs.campFire.pause();
+    event.target.classList.toggle('buttonSongSelected');
+  }
+}
---
+++ src/state.js
@@ -0,0 +1,7 @@
+export default{
+  
+  //songs
+  isMute: true,
+  nameSong: "",
+  
+}
